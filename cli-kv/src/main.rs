f5f9eb8ba6339b07aa21f13eb14e101f9c3fde1c
Jonathan <jonathanthomasv6@gmail.com>
2026-07-25 16:36:07 -0400

    Update CLI prompts for consistency and clarity

--- cli-kv/src/main.rs
+++ cli-kv/src/main.rs
@@ -1,8 +1,8 @@
 use std::io::{self,Write};
 
 fn main(){
-    println!("Simple Rust KV store");
-    println!("Type exit to close the program");
+    println!("Simple Rust KV Store CLI");
+    println!("Type EXIT to close the program");
 
     loop{
         print!("kv> ");
@@ -30,7 +30,18 @@ fn main(){
     if input.eq_ignore_ascii_case("EXIT"){
         println!("Goodbye");
         break;
-    }   
+    }
+    
+    if input.eq_ignore_ascii_case("HELP"){
+        println!("SET key value \n
+                GET key \n
+                DELETE key\n
+                EXISTS key\n
+                COUNT\n
+                CLEAR\n
+                EXIT")
+
+    }
 
     println!("You entered:{}",input);
     }
